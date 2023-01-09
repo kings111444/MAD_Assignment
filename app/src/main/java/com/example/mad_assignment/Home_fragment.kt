cdf237dac5a0fcf79f23bdee3c8e2e53a4b145df
kings111444 <kings111555@gmail.com>
2023-01-10 05:29:34 +0800

    basic menu done

--- app/src/main/java/com/example/mad_assignment/Home_fragment.kt
+++ app/src/main/java/com/example/mad_assignment/Home_fragment.kt
@@ -37,6 +37,8 @@ class Home_fragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home_fragment, container, false)
     }
 
+
+
     companion object {
         /**
          * Use this factory method to create a new instance of
