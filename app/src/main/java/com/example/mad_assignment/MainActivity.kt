37029b153e7e7f1cb43ce09a95b41a309e4d4fc6
kings111444 <kings111555@gmail.com>
2023-01-13 05:55:17 +0800

    Pedometer part is done

--- app/src/main/java/com/example/mad_assignment/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment/MainActivity.kt
@@ -1,21 +1,132 @@
 package com.example.mad_assignment
 
+import android.Manifest
 import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.mad_assignment.DBHelper
+import java.lang.Math.sqrt
 import kotlin.properties.Delegates
 
-class MainActivity : Application() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    var dbHelper: DBHelper by Delegates.notNull()
+    private var magnitudePreviousStep = 0.0
+    private val previousTotalSteps = 0f
+    private var totalSteps = 0f
+    private var running: Boolean = false
+    private var sensorManager: SensorManager? = null
+    private val ACTIVITY_RECOGNITION_REQUEST_CODE: Int = 100
 
-    override fun onCreate() {
-        //intantiate the db-helper when the app is created
-        dbHelper = DBHelper(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(isPermissionGranted()){
+            requestPermission()
+        }
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
+    override fun onSensorChanged(event: SensorEvent?) {
+        var steptaken = findViewById<TextView>(R.id.step_current)
+
+        if(event!!.sensor.type == Sensor.TYPE_ACCELEROMETER){ // For phone with accelermeter sensor
+            val xaccel: Float = event.values[0]
+            val yaccel: Float = event.values[1]
+            val zaccel: Float = event.values[0]
+            val magnitude: Double =
+                kotlin.math.sqrt((xaccel * xaccel + yaccel * yaccel + zaccel * zaccel).toDouble())
+            val magnitudeDelta: Double = magnitude - magnitudePreviousStep
+            magnitudePreviousStep = magnitude
+
+            if(magnitudeDelta > 6){
+                totalSteps++
+            }
+            val step: Int = totalSteps.toInt()
+            steptaken.text = step.toString()
+
+        }else{
+            if(running){
+                totalSteps = event!!.values[0]
+                val currentStep = totalSteps.toInt() - previousTotalSteps.toInt()
+                steptaken.text = currentStep.toString()
+
+            }
+        }
+
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        val detectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        when{
+            countSensor != null -> {
+                sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI)
+            }
+            detectorSensor != null -> {
+                sensorManager.registerListener(this, detectorSensor, SensorManager.SENSOR_DELAY_UI)
+            }
+            accelerometer != null -> {
+                sensorManager.registerListener(this,accelerometer, SensorManager.SENSOR_DELAY_UI)
+            }
+            else -> {
+                Toast.makeText(this,"Your device is not compatible", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        running = false
+        sensorManager?.unregisterListener(this)
+    }
+
+    private fun requestPermission() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACTIVITY_RECOGNITION),ACTIVITY_RECOGNITION_REQUEST_CODE)
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(this,Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            ACTIVITY_RECOGNITION_REQUEST_CODE -> {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+
+                }
+            }
+        }
+    }
 }
 
+//From https://www.youtube.com/watch?v=sOGmivei73I
