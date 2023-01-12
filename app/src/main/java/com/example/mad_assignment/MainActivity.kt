fcdc90d552264e56aa40e5982da8f586d99bb96f
kings111444 <kings111555@gmail.com>
2023-01-12 22:34:50 +0800

    redo the whole app because i can't use fragment to build any function

--- app/src/main/java/com/example/mad_assignment/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment/MainActivity.kt
@@ -1,50 +1,21 @@
 package com.example.mad_assignment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Application
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
-import android.widget.PopupMenu
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.lang.Exception
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mad_assignment.DBHelper
+import kotlin.properties.Delegates
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Application() {
 
-    lateinit var bottomNav : BottomNavigationView
+    var dbHelper: DBHelper by Delegates.notNull()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        loadFragment(Home_fragment())
-        bottomNav = findViewById(R.id.bottomNav)
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.Home -> {
-                    loadFragment(Home_fragment())
-                    true
-                }
-                R.id.Walk -> {
-                    loadFragment(Walk_fragment())
-                    true
-                }
-                R.id.Note -> {
-                    loadFragment(Note_fragment())
-                    true
-                }
-                else -> {
-                    false
-                }
-            }
-        }
-    }
-    private  fun loadFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container,fragment)
-        transaction.commit()
+    override fun onCreate() {
+        //intantiate the db-helper when the app is created
+        dbHelper = DBHelper(this)
     }
 
 }
 
-// from https://www.geeksforgeeks.org/bottom-navigation-bar-in-android-using-kotlin/
